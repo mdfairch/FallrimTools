bf209e9214870a365287beeedbc14ce6ba987d2e
MarkDF <markdfairchild@gmail.com>
2022-11-13 17:34:22 -0800

    Packing files. Script requires 7zip.

---
+++ pack/Build.ps1
@@ -0,0 +1,13 @@
+﻿cd $PSScriptRoot
+cd ..
+
+$7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
+$date = Get-Date -Format "yyyy-MM-dd"
+
+if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
+    throw "7 zip file '$7zipPath' not found"
+}
+
+Set-Alias 7zip $7zipPath
+7zip a -bsp1 "-xr@pack\7z.exclude.txt" "pack\FallrimTools $date.7z" "@pack\7z.listfile.txt"
+pause
---
+++ pack/ReSaver.bat
@@ -0,0 +1,1 @@
+javaw -d64 -Xms512m -Xmx4g -jar target\ReSaver.jar %1
